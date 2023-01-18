dc882f12ea9b821a9be03a037834d60c4f43bd91
Atik Shaikh <Atik.Shaikh@emtecinc.com>
2023-01-18 19:07:45 +0530

    Default Templates

---
+++ Nx-Dev/packages/component-app/src/components/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Button = () => {
+  return (
+    <div>
+        <button> Click me !</button>
+    </div>
+  )
+}
+
---
+++ Nx-Dev/packages/shell-app/config-overrides.js
@@ -0,0 +1,37 @@
+// config-overrides.js
+// see: https://github.com/timarney/react-app-rewired
+ 
+const path = require("path");
+const fs = require("fs");
+ 
+const rewireBabelLoader = require("react-app-rewire-babel-loader");
+ const {override} = require("react-app-rewired");
+// helpers
+ 
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+ 
+module.exports = function override(config, env) {
+
+  //Setting the port to 4000
+  // config.devServer.port = 4000;
+ 
+  // white-list some npm modules to the babel-loader pipeline
+  // see: https://webpack.js.org/configuration/module/#rule-include
+ 
+  config = rewireBabelLoader.include(
+    config,
+    resolveApp("../component-app")
+  );
+ 
+  // black-list some modules from the babel-loader pipeline
+  // see: https://webpack.js.org/configuration/module/#rule-exclude
+ 
+  config = rewireBabelLoader.exclude(
+    config,
+    /(node_modules|bower_components)/
+  );
+ 
+  return config;
+ 
+};
