f86dcb55eb06c3d3bb981bd1fc75f3da1b5c998b
Atik Shaikh <Atik.Shaikh@emtecinc.com>
2023-01-20 07:19:21 +0530

    Fixes

---
+++ Nx-Dev/packages/shell-app/src/ShellApp.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+// import { App } from 'team-a'
+import { App } from 'redux-app';
+
+const ShellApp = () => {
+  return (
+    <div>
+      I'm from the <code>ReduxJS app</code>
+      {/* <Button /> */}
+      <App />
+      {/* <br />
+      <App /> */}
+    </div>
+  );
+};
+
+export default ShellApp;
